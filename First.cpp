e3f15518410b79876c459174c7ad5f6c5f12a089
Samriddh-18102002 <138193938+Samriddh-18102002@users.noreply.github.com>
2023-08-27 16:02:15 +0530

    Create First.cpp

---
+++ First.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  int a = 123;
+
+  cout << a << endl;
+
+  char b = 'v';
+  cout << b << endl;
+
+  int size = sizeof(a);
+  cout << "Size of a is :" << size << endl;
+
+  int z = 'z';
+  cout << z << endl;
+
+  unsigned int c = -123;
+  cout << c << endl;
+
+  float f = 2.0 / 5.0;
+  cout << f << endl;
+}
